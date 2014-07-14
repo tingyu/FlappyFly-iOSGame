ecb94cb4110e1668677c057423d28aec9ab38677
tingyu <ytcleartime@gmail.com>
2014-07-13 23:20:12 -0700

    add ground loop

--- Source/MainScene.m
+++ Source/MainScene.m
@@ -12,9 +12,26 @@ static const CGFloat scrollSpeed = 80.f;
 @implementation MainScene {
     CCSprite *_hero;
     CCPhysicsNode *_physicsNode;
+    CCNode *_ground1;
+    CCNode *_ground2;
+    NSArray *_grounds;
+}
+- (void)didLoadFromCCB {
+    _grounds = @[_ground1, _ground2];
 }
 - (void)update:(CCTime)delta {
     _hero.position = ccp(_hero.position.x + delta * scrollSpeed, _hero.position.y);
     _physicsNode.position = ccp(_physicsNode.position.x - (scrollSpeed *delta), _physicsNode.position.y);
+    // loop the ground
+    for (CCNode *ground in _grounds) {
+        // get the world position of the ground
+        CGPoint groundWorldPosition = [_physicsNode convertToWorldSpace:ground.position];
+        // get the screen position of the ground
+        CGPoint groundScreenPosition = [self convertToNodeSpace:groundWorldPosition];
+        // if the left corner is one complete width off the screen, move it to the right
+        if (groundScreenPosition.x <= (-1 * ground.contentSize.width)) {
+            ground.position = ccp(ground.position.x + 2 * ground.contentSize.width, ground.position.y);
+        }
+    }
 }
 @end
